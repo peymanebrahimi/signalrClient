d51244d0b1a20afadbd533ba3516bc2f7e27911e
peymanebrahimi <peyman.ebrahimi@gmail.com>
2021-09-24 11:53:29 +0330

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   chatHub:"http://185.7.212.235:83/chathub",
-  serverUrl: 'http://localhost:5000',
+  serverUrl: 'http://185.7.212.235:83',
   loginUrl: window.location.origin + "/"
 };
